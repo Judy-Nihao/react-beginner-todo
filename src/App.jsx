73a8d1be47ecfa64813998013d4022323d4283e4
JudySays.Nihao <judysays.nihao@gmail.com>
2024-04-08 00:37:09 +0800

    render li element by todos state and set toggleTodo to re-render li

--- src/App.jsx
+++ src/App.jsx
@@ -6,32 +6,53 @@ import './App.css'
 
 function App() {
 
-  const [todo, setTodo] = useState('');
+  const [newItem, setNewItem] = useState('');
   const [todos, setTodos] = useState([]);
   
-  const updateTodo = (e) => {
-    setTodo(e.target.value);
+
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    setTodos((prev)=>{
+      return [...prev,{
+        title: newItem,
+        id: crypto.randomUUID(),
+        completed: false
+      }]
+    })
+
+    setNewItem('');
   }
+
+  const toggleTodo = (id, completed) => {
+    setTodos( prevTodos => {
+      return prevTodos.map((todo) => {
+        if (id === todo.id ){
+          return {...todo, completed: completed}
+        }
+        return todo
+      })
+    })
+  }
+
+  console.log(todos);
 
   return (
     <div className="container">
-      <form className='new-item-form'>
+      <form className='new-item-form' onSubmit={handleSubmit} >
           <div className='form-row'>
             <label htmlFor='item' className='form-title'>New Item</label>
-            <input type='text' id='item' value={todo} onChange={updateTodo}/>
+            <input type='text' id='item' value={newItem} onChange={ e => setNewItem(e.target.value)}/> 
           </div>
           <button className="add">Add</button>
       </form>
       <h1 className="header">To Do List</h1>
       <ul className="list">
-        <li>
-          <label><input type="checkbox" />item 1</label>
+        {todos.map((todo)=>{
+          return  <li key={todo.id}>
+          <label><input type="checkbox" checked={todo.completed} onChange={e => toggleTodo(todo.id, e.target.checked)}/>{todo.title}</label>
           <button className="btn btn-danger">Delete</button>
         </li>
-        <li>
-          <label><input type="checkbox" />item 2</label>
-          <button className="btn btn-danger">Delete</button>
-        </li>
+        })}
       </ul>
     </div>
   )
